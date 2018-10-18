6336a710c1cf477737d8118ea89ee2752515c714
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 00:33:36 -0400

    Saving merge.R

--- merge.R
+++ merge.R
@@ -1,5 +1,5 @@
 
-library("ggplot")
+library("ggplot2")
 library("ggmap")
 #2)	Copy the USArrests dataset into a local variable (similar to HW 2)
 b <- (USArrests) #USAressts dataset is loaded in a local variable
