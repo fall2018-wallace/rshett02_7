0f1442b58e8c27af5eb2e5ddcb17117ddeb734bc
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 00:06:11 -0400

    Saving merge.R

--- merge.R
+++ merge.R
@@ -1,6 +1,8 @@
 
 #2)	Copy the USArrests dataset into a local variable (similar to HW 2)
 b <- (USArrests) #USAressts dataset is loaded in a local variable
+mergedf <- merge(a, b, by.x = "stateName", by.y = 0) #a merged dataset is created 
+
 
 stateArea <- state.area
 statecenter <- state.center
@@ -28,10 +30,10 @@ map.murder<-map.murder + expand_limits(x=us$long, y= us$lat)
 map.murder<- map.murder + coord_map() +ggtitle("state Murder")
 map.murder
 
-map.murder<- map.murder + geom_point(aes(x=x,y=y,size=population))
-map.murder
+map.murder1<- map.murder + geom_point(aes(x=x,y=y,size=population))
+map.murder1
 
 latlon <- geocode(source = "dsk","nyc,new york,ny")
 latlon
-map.murder1<-map.murder+ xlim(-84,-54)+ ylim(30,50)
-map.murder1
+map.murder2<-map.murder+ xlim(-84,-54)+ ylim(30,50)
+map.murder2
